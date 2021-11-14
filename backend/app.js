286c87bcd63099d1ecf2660181676a099862ea51
kylehillman <krhillman1234@gmail.com>
2021-11-14 02:03:39 -0600

    added backend code in express

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.get('/currenttime', function (req,res){
+res.send('<h1>', Date, '</h1>');
+});
+
+app.get('/', function (req, res) {
+    res.send('<form action="/store-user" method="POST"><label><input type="text" name="username"></label></form>');
+});
+
+app.post('/store-user', function(req,res){
+    const userName = req.body.username;
+    const filePath = path.join(__dirname, 'data','users.JSON');
+    const fileData= fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    existingUsers.push(userName);
+    fs.writeFileSync(filePath,JSON.stringify(existingUsers));
+    res.send('<h1>Username stored!</h1>');
+});
+
+app.get('/users', function(req,res){
+    const filePath = path.join(__dirname, 'data', 'users.JSON');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+    let responseData = '<ul>';
+    for (const user of existingUsers) {
+        responseData += '<li>' + user + '</li>';
+    };
+    responseData+='</ul>';
+    res.send(responseData);
+});
+app.listen(3000)
